d57be4f9fe2cff66d3c6ca8ed9da158ccc18ef02
Doug Walter <doug@kosmojo.com>
2015-07-16 16:44:19 +0200

    basic crud api

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Beer = require('./app/models/beer');
 
 //configuration
 
@@ -13,6 +17,61 @@ var db = require('./config/db');
 var port = process.env.PORT || 9000;
 
 var router = express.Router();
+
+router.route('/beers')
+	.post(function(req, res){
+		var beer = new Beer();
+		beer.name = req.body.name;
+
+		beer.save(function(err){
+			if(err)
+				res.send(err);
+			res.json({message: "Beer created!"});
+		});
+	})
+	.get(function(req,res){
+		Beer.find(function(err, beers){
+			if(err)
+				res.send(err);
+			res.json(beers);
+		});
+	});
+
+router.route('/beers/:beer_id')
+	.get(function(req, res){
+		Beer.findById(req.params.beer_id, function(err, beer){
+			if (err)
+				res.send(err);
+			res.json(beer);
+		});
+	})
+	.put(function(req, res){
+		Beer.findById(req.params.beer_id, function(err, beer){
+			if (err)
+				res.send(err);
+			beer.name = req.body.name;
+
+			beer.save(function(err){
+				if (err)
+					res.send(err)
+				res.json({message: 'beer updated'});
+			});
+		});
+	})
+	.delete(function(req, res){
+		Beer.remove({
+			_id: req.params.beer_id
+		}, function(err, bear){
+				if (err)
+					res.send(err);
+				res.json({message: "Beer deleted!"});
+		});
+	});
+
+router.use(function(req, res, next) {
+    console.log('Something is happening.');
+    next();
+});
 
 router.get('/', function(req,res){
 	res.json({message: 'hooray welcome to our api!'});
